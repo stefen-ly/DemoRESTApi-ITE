15dcf971389e297827e189a28791e9bca02f27b7
stefen-ly <vanlythouern51@gmail.com>
2026-06-11 22:40:59 +0700

    initail commit

---
+++ src/test/java/org/example/restfullapidemo/RestfullapidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restfullapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullapidemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
